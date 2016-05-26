23b943017b53c0aa9a8143ccd26834e507200c59
Ciarán Murphy <Ciarn Murphy>
2016-05-26 11:14:00 +0100

    Script me baby one more time

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function resetOutput(){
+	document.getElementById("test").innerHTML = "( ͡° ͜ʖ ͡°)";
+};
+
 function ayyLmao(){ 
 	document.getElementById("test").innerHTML = "Ayy Lmao.";
+	setTimeout(resetOutput(), 3000);
 };
+
