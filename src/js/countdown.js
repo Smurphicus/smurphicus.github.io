c1233ec44ba8d457b918b64fae856b5055dd1c47
Smurphicus <murphyspiderpig@gmail.com>
2024-08-22 10:18:51 +0100

    Updated arrival times

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,15 +1,19 @@
 const arrivalTimes = {
-    "smurphCountdown" : new Date('2024-08-25T22:30:00').getTime(),
-    "rowanCountdown" : new Date('2024-08-25T22:30:00').getTime(),
+    "smurphCountdown" : new Date('2024-08-25T04:00:00').getTime(),
+    "rowanCountdown" : new Date('2024-08-25T04:00:00').getTime(),
     "joshCountdown" : new Date('2024-08-22T01:05:00').getTime(),
     "dayMattCountdown" : new Date('2024-08-22T01:05:00').getTime(),
-    "chezCountdown" : new Date('2024-08-26T20:05:00').getTime(),
+    "chezCountdown" : new Date('2024-08-26T11:05:00').getTime(),
     "craftCountdown" : new Date('2024-08-25T11:05:00').getTime()
 }
 
 function getCountdownString(targetDate) {
     const now = new Date().getTime();
     const timeDifference = targetDate - now;
+
+    if (timeDifference <= 0) {
+        return "Arrived!"
+    }
 
     const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
